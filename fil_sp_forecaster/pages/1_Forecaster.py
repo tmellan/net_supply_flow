83ebe7c05b0089071ddea7e4e713e4f98709c4f2
Kiran Karra <kiran.karra@gmail.com>
2023-09-01 11:14:32 -0700

    showing less history

--- fil_sp_forecaster/pages/1_Forecaster.py
+++ fil_sp_forecaster/pages/1_Forecaster.py
@@ -230,7 +230,7 @@ def main():
         layout="wide",
     )
     current_date = date.today() - timedelta(days=3)
-    mo_start = min(current_date.month - 1 % 12, 1)
+    mo_start = max(current_date.month - 1 % 12, 1)
     start_date = date(current_date.year, mo_start, 1)
     forecast_length_days=365
     end_date = current_date + timedelta(days=forecast_length_days)
