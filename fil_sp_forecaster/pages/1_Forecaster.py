3f20d716847f070105537cf20380f55ed73b642e
Thomas <mellan.thomas@gmail.com>
2023-09-07 18:32:52 +0100

    Update 1_Forecaster.py

--- fil_sp_forecaster/pages/1_Forecaster.py
+++ fil_sp_forecaster/pages/1_Forecaster.py
@@ -206,7 +206,7 @@ def forecast_economy(start_date=None, current_date=None, end_date=None, forecast
     # filp_scaling_cost_pct = st.session_state['filp_scaling_cost_pct']
 
     lock_target = st.session_state['lock_target_slider']
-    sector_duration_days = 360
+    sector_duration_days = st.session_state['av_dur_slider']
     
     # get offline data
     t2 = time.time()
@@ -279,6 +279,9 @@ def main():
         st.slider("FIL+ Rate (Percentage)", min_value=10, max_value=99, value=smoothed_last_historical_fil_plus_pct, step=1, format='%d', key="fpr_slider",
                 on_change=forecast_economy, kwargs=forecast_kwargs, disabled=False, label_visibility="visible")
         st.slider("Lock Target", min_value=0.1, max_value=0.9, value=0.3, step=0.01, format='%.2f', key="lock_target_slider",
+                on_change=forecast_economy, kwargs=forecast_kwargs, disabled=False, label_visibility="visible")
+        
+        st.slider("Sector Duration", min_value=180, max_value=540, value=360, step=10, format='%d', key="av_dur_slider_slider",
                 on_change=forecast_economy, kwargs=forecast_kwargs, disabled=False, label_visibility="visible")
         
         st.button("Forecast", on_click=forecast_economy, kwargs=forecast_kwargs, key="forecast_button")
