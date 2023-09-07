b37e9f740654271f0f441cf50a79a6b61de4e866
Thomas <mellan.thomas@gmail.com>
2023-09-07 18:17:36 +0100

    Update 1_Forecaster.py

--- fil_sp_forecaster/pages/1_Forecaster.py
+++ fil_sp_forecaster/pages/1_Forecaster.py
@@ -252,7 +252,7 @@ def main():
     current_date = date.today() - timedelta(days=3)
     mo_start = max(current_date.month - 1 % 12, 1)
     start_date = date(current_date.year, mo_start, 1)
-    forecast_length_days=365
+    forecast_length_days=(365*3)
     end_date = current_date + timedelta(days=forecast_length_days)
     forecast_kwargs = {
         'start_date': start_date,
