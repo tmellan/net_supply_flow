cbfb5e11465efcfa5105c321f5c37820fcf8bfae
Thomas <mellan.thomas@gmail.com>
2023-09-07 17:56:52 +0100

    Update 1_Forecaster.py

--- fil_sp_forecaster/pages/1_Forecaster.py
+++ fil_sp_forecaster/pages/1_Forecaster.py
@@ -64,10 +64,8 @@ def plot_panel(scenario_results, baseline, start_date, current_date, end_date):
     pledge_dff['date'] = pd.to_datetime(du.get_t(start_date, forecast_length=pledge_dff.shape[0]))
 
     supplyflow_dff = pd.DataFrame()
-    # supplyflow_dff['StatusQuo'] = status_quo_results['circ_supply'].diff().rolling(7).median().dropna() / 1_000_000
-    # supplyflow_dff['date'] = pd.to_datetime(du.get_t(start_date, end_date=end_date)).tail(supplyflow_dff.shape[0])
-
-    supplyflow_dff['StatusQuo'] = status_quo_results['circ_supply'].diff().rolling(7).median() / 1_000_000
+    # .diff().rolling(7).median() / 1_000_000
+    supplyflow_dff['StatusQuo'] = status_quo_results['circ_supply']
     supplyflow_dff['date'] = pd.to_datetime(du.get_t(start_date, end_date=end_date))
     supplyflow_dff.dropna(inplace=True)
     
