e9e66a75ad4c9861841f10534df8d91a49e62c4e
Thomas <mellan.thomas@gmail.com>
2023-09-07 17:54:49 +0100

    Update 1_Forecaster.py

--- fil_sp_forecaster/pages/1_Forecaster.py
+++ fil_sp_forecaster/pages/1_Forecaster.py
@@ -69,6 +69,7 @@ def plot_panel(scenario_results, baseline, start_date, current_date, end_date):
 
     supplyflow_dff['StatusQuo'] = status_quo_results['circ_supply'].diff().rolling(7).median() / 1_000_000
     supplyflow_dff['date'] = pd.to_datetime(du.get_t(start_date, end_date=end_date))
+    supplyflow_dff.dropna(inplace=True)
     
     # with col1:
     power_df = pd.melt(power_dff, id_vars=["date"], 
