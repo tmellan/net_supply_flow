9c02f91df2a921edcfa893597681c86ec02772e6
Thomas <mellan.thomas@gmail.com>
2023-09-14 00:28:40 +0800

    Update Filecoin_CryptoEconomics.py

--- fil_sp_forecaster/Filecoin_CryptoEconomics.py
+++ fil_sp_forecaster/Filecoin_CryptoEconomics.py
@@ -11,8 +11,8 @@ st.sidebar.success("Select a Page above.")
 
 st.markdown(
     """
-    ### Filecoin CryptoEconomics Explorer
-    This web-app uses a digital twin of the Filecoin Economy to forecast power, minting, and pledge, given some user defined assumptions about storage provider behavior.
+    ### Filecoin Deflationary Supply Explorer
+    This web-app uses a digital twin of the Filecoin Economy to explore change in net supply flow, given some user defined assumptions about storage provider behavior.
     
     To learn more about the model assumptions and design, please refer to:
      1. Our [GitHub repository](https://github.com/protocol/mechafil-jax).
@@ -28,6 +28,9 @@ st.markdown(
      - **RB Onboarding Rate** - This is the amount of raw-byte power that is onboarded every day onto the network, in units of PiB/day.
      - **Renewal Rate** - This is the percentage of sectors which are expiring that are renewed.
      - **FIL+ Rate** - This is the percentage of onboarded data that is FIL+.
+     - **Av. Sec. Dur.** - This is the average sector duration in days. 
+     - **Lock Target** - This is percentage of circulating supply the network targets locking.
+     - **Burn Factr** - Factor change increasing or decreasing the current burn rate. 
     
     Once these values are configured, click the "Forecast" button. The digital-twin runs in the background, taking into account both historical data pulled directly from the Filecoin blockchain and the mechanistic laws defining Filecoin's to
     forecast pledge, block rewards, and network power. The results are displayed in the form of a graph, which you can download as a PNG file by clicking the "Download" button.
