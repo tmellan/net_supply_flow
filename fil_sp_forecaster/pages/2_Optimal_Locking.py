88087eab7fee2f1cf403df2ba1c71b374d8ee2ad
Thomas <mellan.thomas@gmail.com>
2023-09-14 01:09:26 +0800

    Update 2_Optimal_Locking.py

--- fil_sp_forecaster/pages/2_Optimal_Locking.py
+++ fil_sp_forecaster/pages/2_Optimal_Locking.py
@@ -54,15 +54,15 @@ st.set_page_config(
 
 with st.sidebar:
     st.slider(
-        "SP costs as a \% of rewards", min_value=50, max_value=95, value=60, step=1, key="cost_pct_rewards",
+        "SP costs as a \% of rewards", min_value=50, max_value=95, value=90, step=1, key="cost_pct_rewards",
         on_change=plot_ROI
     )
     st.slider(
-        "\% of costs in paid in fiat", min_value=50, max_value=95, value=5, step=1, key="xr_locking_sensitivity",
+        "\% of costs in paid in fiat", min_value=50, max_value=95, value=80, step=1, key="xr_locking_sensitivity",
         on_change=plot_ROI
     )
     st.slider(
-        "XR locking sensitivity", min_value=1, max_value=95, value=60, step=1, key="pct_fiat_cost",
+        "XR locking sensitivity", min_value=1, max_value=25, value=10, step=1, key="pct_fiat_cost",
         on_change=plot_ROI
     )
     st.button("Run", on_click=plot_ROI)
