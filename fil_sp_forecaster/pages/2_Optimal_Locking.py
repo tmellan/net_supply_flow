8845efdf71e35919fd88ec9606f72ba8ccc1d61c
Thomas <mellan.thomas@gmail.com>
2023-09-13 16:45:36 +0800

    Update 2_Optimal_Locking.py

--- fil_sp_forecaster/pages/2_Optimal_Locking.py
+++ fil_sp_forecaster/pages/2_Optimal_Locking.py
@@ -32,7 +32,7 @@ def plot_ROI():
     
     plot_df = plot_df.melt('TL', var_name='ROI', value_name='Value')
     chart = alt.Chart(plot_df).mark_line().encode(
-        x=alt.X('TL', title='% of supply locked'),
+        x=alt.X('TL', title='\% of supply locked'),
         y='Value',
         # color='ROI'
     ).properties(
