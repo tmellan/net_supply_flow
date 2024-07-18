21989787e2687a1607c679824fe51bf482746112
Tom <mellan.thomas@gmail.com>
2024-07-18 16:21:04 +0300

    Update 1_Supply_Forecaster.py

--- fil_sp_forecaster/pages/1_Supply_Forecaster.py
+++ fil_sp_forecaster/pages/1_Supply_Forecaster.py
@@ -277,7 +277,8 @@ def main():
         layout="wide",
     )
     current_date = date.today() - timedelta(days=3)
-    mo_start = max(current_date.month - 1 % 12, 1)
+    # mo_start = max(current_date.month - 1 % 12, 1)
+    mo_start = max(current_date.month % 12, 1)
     start_date = date(current_date.year, mo_start, 1)
 
     forecast_length_days=(365*1)
